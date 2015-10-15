35895894e13c82c73e13b666d932e0cc50c96e63
Eduladder <arunwebber@gmail.com>
2015-10-15 11:08:59 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 	define("DB_HOST","localhost");
-	define("DB_USERNAME","root");
-	define("DB_PASSWORD","eduladder");
+	define("DB_USERNAME","");
+	define("DB_PASSWORD","");
 	define("DB_DATABASE","teacher_review");
 ?>
